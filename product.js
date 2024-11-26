2aabc62f846fc5a8c997d7ed3f5907060143bcff
mrabiimran <mrabiimran@gmail.com>
2024-11-26 16:13:19 +0400

    added

---
+++ product.js
@@ -0,0 +1,55 @@
+let products = [
+    {
+   "id": 1001,
+    "title": "Cat Food, 25lb bag",
+    "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat."
+    ,
+    "price": 20.00,
+    "image": "images/bird.jpg",
+    "availableInventory": 10,
+    "rating": 2
+    },
+{
+   "id": 1002,
+    "title": "sami",
+    "description": " sami your cat can play with for a very <strong>long</strong> time!",
+     "price": 2.99,
+    "image": "images/cow2.jpg",
+    "availableInventory": 7,
+    "rating": 3
+},
+{
+    "id": 1003,
+    "title": "Cat",
+    "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat."
+    ,
+    "price": 3.88,
+    "image": "images/loin.jpg",
+    "availableInventory": 10,
+    "rating": 4
+},
+{
+    "id": 1004,
+    "title": "yarn",
+    "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat."
+    ,
+    "price": 10.01,
+    "image": "images/goat.jpg",
+    "availableInventory": 10,
+    "rating": 2
+},
+{
+ 
+ 
+"id": 1005,
+    "title": "Cat Food",
+    "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat."
+    ,
+    "price": 20.00,
+    "image": "images/goat.jpg",
+    "availableInventory": 10,
+    "rating": 5
+}
+ 
+   
+   ]
